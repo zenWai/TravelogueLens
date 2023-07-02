2d08b5c91440112e7386abb8d888ab8d0419ce66
Fernando <ivopresa@gmail.com>
2023-07-03 01:03:46 +0200

    Take Photo with ImagePicker, android

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,24 @@
+import {Button, View} from "react-native";
+import {launchCameraAsync} from "expo-image-picker";
+
+function ImagePicker() {
+    async function takeImageHandler() {
+        const image =
+            await launchCameraAsync({
+                allowsEditing: true,
+                aspect: [16,9],
+                quality: 0.5,
+            });
+        console.log(image);
+    }
+    return (
+        <View>
+            <View>
+
+            </View>
+            <Button title="Take Image" onPress={takeImageHandler}/>
+        </View>
+    );
+}
+
+export default ImagePicker;
