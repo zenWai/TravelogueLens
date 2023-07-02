8a7dddc68bca6ead8734b0c2091bc06b1f27e6fa
Fernando <ivopresa@gmail.com>
2023-07-03 00:18:16 +0200

    App styling

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import {FlatList, StyleSheet, Text, View} from "react-native";
 import PlaceItem from "./PlaceItem";
+import {Colors} from "../../constants/colors";
 
 function PlacesList({ places }) {
     if (!places || places.length === 0) {
@@ -28,5 +29,6 @@ const styles = StyleSheet.create({
     },
     fallbackText: {
         fontSize: 16,
-    }
+        color: Colors.primary200,
+    },
 })
