91b74c20973bae5099e58a7afcda856612b8c046
Fernando <ivopresa@gmail.com>
2023-07-03 00:10:21 +0200

    AddPlace screen and header icon

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <>
-      <StatusBar style={'dark'}/>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces}/>
-          <Stack.Screen name="AddPlace" component={AddPlace}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
+    return (
+        <>
+            <StatusBar style={'dark'}/>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="AllPlaces"
+                        component={AllPlaces}
+                        options={({ navigation }) => ({
+                            headerRight: ({ tintColor }) => (
+                                <IconButton
+                                    icon="add"
+                                    size={24}
+                                    color={tintColor}
+                                    onPress={() => navigation.navigate('AddPlace')}
+                                />
+                            ),
+                        })}
+                    />
+                    <Stack.Screen name='AddPlace' component={AddPlace}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
