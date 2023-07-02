313d6dd532c8130010d5ed9fe4cca00d16ac5777
Fernando <ivopresa@gmail.com>
2023-07-03 00:12:37 +0200

    Titles

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ export default function App() {
                         name="AllPlaces"
                         component={AllPlaces}
                         options={({ navigation }) => ({
+                            title: 'Your Favorite Places',
                             headerRight: ({ tintColor }) => (
                                 <IconButton
                                     icon="add"
@@ -26,7 +27,13 @@ export default function App() {
                             ),
                         })}
                     />
-                    <Stack.Screen name='AddPlace' component={AddPlace}/>
+                    <Stack.Screen
+                        name='AddPlace'
+                        component={AddPlace}
+                        options={{
+                            title: 'Add a new Place',
+                        }}
+                    />
                 </Stack.Navigator>
             </NavigationContainer>
         </>
