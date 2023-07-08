346737550f7a342c50541d60c0e2edb77888fe18
Fernando <ivopresa@gmail.com>
2023-07-08 04:33:49 +0200

    Improved permissions for the map, initial region on userLocation and marker on it. Managing edge cases of location permissions

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,8 @@
 import MapView, {Marker} from "react-native-maps";
-import {Alert, StyleSheet} from "react-native";
+import {ActivityIndicator, Alert, Linking, StyleSheet, View} from "react-native";
 import {useCallback, useLayoutEffect, useState} from "react";
 import IconButton from "../components/UI/IconButton";
+import {getCurrentPositionAsync, PermissionStatus, requestForegroundPermissionsAsync} from 'expo-location';
 
 function Map({ navigation, route }) {
     const initialLocation = route.params && {
@@ -9,14 +10,16 @@ function Map({ navigation, route }) {
         lng: route.params.initialLng,
     }
     const [selectedLocation, setSelectedLocation] = useState(initialLocation);
+    const [isLoading, setIsLoading] = useState(false);
+    const [locationPermissionInformation, setLocationPermissionInformation] = useState(null);
 
 
-    const region = {
+    const [region, setRegion] = useState({
         latitude: initialLocation ? initialLocation.lat : 37.50,
         longitude: initialLocation ? initialLocation.lng : -122.33,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-    };
+    });
 
     function selectLocationHandler(event) {
         if (initialLocation) {
@@ -49,7 +52,7 @@ function Map({ navigation, route }) {
 
     useLayoutEffect(() => {
         //if got initial location header button will not be shown
-        if(initialLocation) {
+        if (initialLocation) {
             return;
         }
         navigation.setOptions({
@@ -63,9 +66,69 @@ function Map({ navigation, route }) {
         });
     }, [navigation, savePickedLocationHandler, initialLocation]);
 
+    const verifyPermissions = async () => {
+        const { status } = await requestForegroundPermissionsAsync();
+        setLocationPermissionInformation(status);
+        return status === PermissionStatus.GRANTED;
+    };
+
+    useLayoutEffect(() => {
+        if (!initialLocation) {
+            const fetchCurrentLocation = async () => {
+                const hasPermission = await verifyPermissions(); // Invoke hasPermission function
+                function openSettings() {
+                    Linking.openSettings();
+                }
+                if (!hasPermission) { // Change the condition here
+                    Alert.alert(
+                        'Insufficient Permissions',
+                        'You need to grant location permissions to use this app',
+                        [
+                            {
+                                text: 'Cancel',
+                                style: 'cancel',
+                            },
+                            {
+                                text: 'Open Settings',
+                                onPress: () => openSettings(),
+                            },
+                        ]
+                    );
+                }
+                setIsLoading(true);
+                try {
+                    const { coords } = await getCurrentPositionAsync({});
+                    setRegion(prevRegion => ({
+                        ...prevRegion,
+                        latitude: coords.latitude,
+                        longitude: coords.longitude
+                    }));
+                    // to place marker
+                    setSelectedLocation({
+                        lat: coords.latitude,
+                        lng: coords.longitude,
+                    });
+                } catch (error) {
+                    console.log('Error fetching current location:', error);
+                } finally {
+                    setIsLoading(false);
+                }
+            };
+
+            fetchCurrentLocation();
+        }
+    }, [initialLocation]);
+    if (isLoading) {
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator style={styles.loading} size="large" color="#000000"/>
+            </View>
+        );
+    }
+
     return <MapView
         style={styles.map}
-        initialRegion={region}
+        region={region}
         onPress={selectLocationHandler}
     >
 
@@ -84,5 +147,13 @@ export default Map;
 const styles = StyleSheet.create({
     map: {
         flex: 1,
-    }
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loading: {
+        alignSelf: 'center',
+    },
 });
